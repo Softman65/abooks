ec4c873814d2bc1fa6df0aa98c45927b31b1d11c
Softman65 <rbenitez2012@gmail.com>
2018-05-01 00:31:10 +0200

    push

--- app.js
+++ app.js
@@ -14,6 +14,38 @@ const mysql = {
 mysql.connection = mysql.engine.createConnection(mysql.credentials);
 mysql.connection.connect();
 
+
+const convertToRecord = function(JsonRecord){
+    const getcodes = function(codes){
+        var _ret = []
+        _.each(codes, function(code){
+            _ret.push(data.bsacode)
+        })
+        return _ret
+    }
+    return {
+        vendorListingid:JsonRecord.vendorListingid,
+        author:JsonRecord.author,
+        title:JsonRecord.title,
+        description:JsonRecord.description,
+        bookCondition:JsonRecord.bookCondition,
+        listingsid:JsonRecord.listingsid,
+        price_currency:JsonRecord.price.currency,
+        price_quantity:JsonRecord.price.$t,
+        quantity_limit:JsonRecord.quantity.limit,
+        quantity_amount:JsonRecord.quantity.amount,
+        publisherName:JsonRecord.publisher.Name,
+        publishYear:JsonRecord.publish.Year,
+        publishYearText:JsonRecord.publish.YearText,
+        bindingText:JsonRecord.bindingText,
+        universalIdentifier_isvalid:JsonRecord.universalIdentifier.isvalid,
+        universalIdentifier_numberType:JsonRecord.universalIdentifier.numberType,
+        universalIdentifier_number:JsonRecord.universalIdentifier.number,
+        buyerSearchAttribute: getcodes(JsonRecord.buyerSearchAttribute)
+    }
+}
+
+
 var fs = require('fs'),
     path = require('path'),
     xmlReader = require('read-xml'),
@@ -33,8 +65,8 @@ xmlReader.readXML(fs.readFileSync(FILE), function (err, data) {
     var parser = require('xml2json');
     var json = JSON.parse(parser.toJson( encoding.convert( data.content, 'utf-8', data.encoding )));
     _.each(json.BookListingInventory.BookListing, function(book){
+        record = convertToRecord(book)
         debugger
-
     })
 });
 
