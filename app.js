2db2a6f511ac9f31940769dec0ccf1721a978232
Softman65 <rbenitez2012@gmail.com>
2018-04-30 23:50:32 +0200

    push

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ console.log('Hello world');
 const mysql = {
     engine: require('mysql'),
     credentials: {
-    host: '35.204.32.143',
+    host: 'localhost',
     user: 'root',
-    password: 'wotanonline',
+    password: 'guatemala016',
     database: 'wotan_dev'
     }
 }
@@ -15,7 +15,8 @@ mysql.connection = mysql.engine.createConnection(mysql.credentials);
 
 var fs = require('fs'),
     path = require('path'),
-    xmlReader = require('read-xml')
+    xmlReader = require('read-xml'),
+    encoding = require("encoding");
 
 var FILE = path.join(__dirname, './20180318_InventarioAbeBooks.xml');
 const cheerio = require('cheerio');
@@ -29,7 +30,7 @@ xmlReader.readXML(fs.readFileSync(FILE), function (err, data) {
     console.log('Decoded xml:', data.content);
 
     var parser = require('xml2json');
-    var json = JSON.parse(parser.toJson(data.content));
+    var json = JSON.parse(parser.toJson( encoding.convert( data.content, 'utf-8', data.encoding )));
     debugger
 });
 
