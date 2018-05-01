7b458cf2c28e10b72d8e92d77e97d20dc5f743c5
Softman65 <rbenitez2012@gmail.com>
2018-05-02 00:25:26 +0200

    push

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.set('port', process.env.PORT || 80);
 
 var server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
-    upgradeDb()
+    //upgradeDb()
 });
 
 
