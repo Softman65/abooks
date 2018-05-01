5ef544c4800c78b54e8f116409182c1c498399eb
Softman65 <rbenitez2012@gmail.com>
2018-05-01 11:35:36 +0200

    PUSH

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
         { Name: "Canada", Id: 2 },
         { Name: "United Kingdom", Id: 3 }
     ];
-    
+
     $("#jsGrid").jsGrid({
         height: "80%",
         width: "100%",
@@ -26,20 +26,31 @@ $(document).ready(function() {
  
         controller: {
             loadData: function(filter) {
-                var startIndex = (filter.pageIndex - 1) * filter.pageSize;
-                return {
-                    data: db.clients.slice(startIndex, startIndex + filter.pageSize),
-                    itemsCount: db.clients.length
-                };
+                var d = $.Deferred();
+ 
+                $.ajax({
+                    url: "http://services.odata.org/V3/(S(3mnweai3qldmghnzfshavfok))/OData/OData.svc/Products",
+                    dataType: "json"
+                }).done(function(response) {
+                    d.resolve(response.value);
+                });
+ 
+                return d.promise();
             }
         },
  
         fields: [
-            { name: "Name", type: "text", width: 150 },
-            { name: "Age", type: "number", width: 50 },
-            { name: "Address", type: "text", width: 200 },
-            { name: "Country", type: "select", items: db.countries, valueField: "Id", textField: "Name" },
-            { name: "Married", type: "checkbox", title: "Is Married" }
+            { name: "Name", type: "text" },
+            { name: "Description", type: "textarea", width: 150 },
+            { name: "Rating", type: "number", width: 50, align: "center",
+                itemTemplate: function(value) {
+                    return $("<div>").addClass("rating").append(Array(value + 1).join("&#9733;"));
+                }
+            },
+            { name: "Price", type: "number", width: 50,
+                itemTemplate: function(value) {
+                    return value.toFixed(2) + "$"; }
+            }
         ]
     });
 
