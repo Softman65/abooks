6900d7b038d4dce482e464d89c7d2d00a4486896
Softman65 <rbenitez2012@gmail.com>
2018-05-01 21:15:34 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
         
                 fields: [
                     { title: "id", name: "idbooks", type: "number", width: 25, visible:false },
-                    { name: "vendorListingid", title:'ref' ,type: "text", width: 30, align: "center" },
+                    { name: "vendorListingid", title:'ref' ,type: "text", width: 50, align: "center" },
                     //{ name: "Rating", type: "number", width: 50, align: "center",
                     //    itemTemplate: function(value) {
                     //        return $("<div>").addClass("rating").append(Array(value + 1).join("&#9733;"));
