fe0f1a6038770705609a6340816aa93a488d165c
Softman65 <rbenitez2012@gmail.com>
2018-05-02 00:56:13 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -40,11 +40,12 @@ $(document).ready(function() {
 
                 pageSize: 18,
                 pageIndex:1,
-                finishLoad: function(loadedData) {
-                    $('.sale.icon').parent().parent().css({color:'red'})
-                },                     // handles the finish of loading data by controller.loadData
+                                     // handles the finish of loading data by controller.loadData
 
                 controller: {
+                    finishLoad: function(loadedData) {
+                        $('.sale.icon').parent().parent().css({color:'red'})
+                    },
                     loadData: function(filter) {
 
                         return $.ajax({
