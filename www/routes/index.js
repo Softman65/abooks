89932911bd07a1fee3dcb8902516eba29e166adf
Softman65 <rbenitez2012@gmail.com>
2018-05-01 12:19:44 +0200

    push

--- www/routes/index.js
+++ www/routes/index.js
@@ -2,26 +2,23 @@
 var express = require('express');
 var router = express.Router();
 
-
+const mysql = {
+    engine: require('mysql'),
+    credentials: {
+        host: 'localhost',
+        user: 'root',
+        password: 'guatemala016',
+        database: 'abooks'
+    }
+}
+mysql.connection = mysql.engine.createConnection(mysql.credentials);
+mysql.connection.connect();
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 router.get('/api/books', function (req, res) {
-    
-    const mysql = {
-        engine: require('mysql'),
-        credentials: {
-            host: 'localhost',
-            user: 'root',
-            password: 'guatemala016',
-            database: 'abooks'
-        }
-    }
-    mysql.connection = mysql.engine.createConnection(mysql.credentials);
-    mysql.connection.connect();
-
-     mysql.connection("SELECT * FROM books LIMIT 1,50", function(err,records) {
+    mysql.connection.query("SELECT * FROM books LIMIT 1,50", function(err,records) {
          res.send(JSON.stringify(records));
          debugger
      //res.send('hi')
