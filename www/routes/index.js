8fec97f4282cb69e6ac3dc0241099549646b8479
Softman65 <rbenitez2012@gmail.com>
2018-05-01 12:40:16 +0200

    push

--- www/routes/index.js
+++ www/routes/index.js
@@ -2,6 +2,18 @@
 var express = require('express');
 var router = express.Router();
 
+function cint(num, opt_infinityBiased) {
+    var m = Math, c = m.ceil, f = m.floor, r = m.round;
+    num = +num;
+    if (opt_infinityBiased) {
+        return num < 0 ? r(num) : f(num);
+    }
+    if (opt_infinityBiased == null) {
+        return num < 0 ? -r(-num) : r(num);
+    }
+    return num < 0 ? c(num) : f(num);
+}
+
 const mysql = {
     engine: require('mysql'),
     credentials: {
@@ -17,9 +29,18 @@ mysql.connection.connect();
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
-router.get('/api/books', function (req, res) {
+router.get('/api/books/page', function (req, res) {
     mysql.connection.query("SELECT * FROM books LIMIT "+(req.query.total*(req.query.page-1)+1)+","+req.query.total, function(err,records) {
          res.send(JSON.stringify(records));
+         //debugger
+     //res.send('hi')
+     })
+ });
+ router.get('/api/books/total', function (req, res) {
+    mysql.connection.query("SELECT count(*) as total, FROM books ", function(err,records) {
+        records[0].pages = cint(records[0].pages / req.query.elems)
+        records[0].elemsperpage = req.query.elems
+        res.send(JSON.stringify(records));
          //debugger
      //res.send('hi')
      })
