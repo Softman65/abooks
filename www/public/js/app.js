43f2309569cccb3a5a43115be97887587939894c
Softman65 <rbenitez2012@gmail.com>
2018-05-02 13:01:27 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -62,7 +62,14 @@ $(document).ready(function() {
                       //text.push(value + " : " + getData[value])
                     });
                     $('#edit .header.book').html('<span><span class="left green">'+getData.title+'</span><span class="right">'+getData.vendorListingid+'-<span class="red">'+(getData._loc==null?'?':getData._loc)+'</span></span></span>')
-                    $('#edit').modal('show')
+                    $('#edit').modal({
+                      onDeny    : function(){
+                        window.alert('Wait not yet!');
+                        return false;
+                      },
+                      onApprove : function() {
+                        window.alert('Approved!');
+                      }}).modal('show')
                     $('#edit.ui.approve.button').html('Modificar').click(function(){
 
                     })
