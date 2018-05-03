872490137f46b61d5c44a0cdc60e080e7d7f4452
Softman65 <rbenitez2012@gmail.com>
2018-05-03 20:51:25 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     function getFormData($form){
         var unindexed_array = $form.serializeArray();
         var indexed_array = {};
-    
+        debugger
+        
         $.map(unindexed_array, function(n, i){
             indexed_array[n['name']] = n['value'];
         });
