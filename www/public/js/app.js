af2a3933d9a09c68256ffd59f63fa7914fb5d226
Softman65 <rbenitez2012@gmail.com>
2018-05-03 18:49:44 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -102,7 +102,7 @@ $(document).ready(function() {
                                 if($('#edit input[name="'+value+'"]').attr('type')!='hidden'){
                                     $('#edit input[name="'+value+'"]').val(getData[value])
                                 }else{
-                                    $('#edit .'+value+' .text').html(getData[value])
+                                    $('#edit .'+value+' .text').removeClass('default').html(getData[value])
                                 }
                             }
                         }
