0b4b1cb8e2195a263e5a3bcc8463779adb39232d
Softman65 <rbenitez2012@gmail.com>
2018-05-04 20:50:47 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -19,20 +19,7 @@ $(document).ready(function() {
         })
         return _ret!={}?_ret:null
     }
-    var clients = [
-        { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-        { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-        { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-        { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-        { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
-    ];
- 
-    var countries = [
-        { Name: "", Id: 0 },
-        { Name: "United States", Id: 1 },
-        { Name: "Canada", Id: 2 },
-        { Name: "United Kingdom", Id: 3 }
-    ];
+
     $.ajax({
         url: "/api/books/tables",
         dataType: "json"
@@ -216,6 +203,7 @@ $(document).ready(function() {
     $('.ui.menu .item').on('click', function() {
 
         if($(this).hasClass('new')){
+            
             $('#edit .ui.approve.button').addClass("disabled").html('Crear')
             $('#edit .header.book').html('Nuevo Libro')
             $('#edit').modal().modal('show')
