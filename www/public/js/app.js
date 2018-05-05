a6914669ca90168971d98977ae5e28fd7b9bbfde
Softman65 <rbenitez2012@gmail.com>
2018-05-05 21:36:32 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -116,7 +116,7 @@ $(document).ready(function() {
 
         
         $(' input.numeric').keydown(function(event){
-            return ( event.which >48 &&  event.which <63 || event.which ==13) 
+            return ( event.which >48 &&  event.which <63 || event.which ==13 || event.which==46 || event.which==8) 
         })
 
         $('.validate input').keyup(function(event){
