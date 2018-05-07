b4f49894491886181b795a72897dc7a212ec89d7
Softman65 <rbenitez2012@gmail.com>
2018-05-07 10:18:36 +0200

    push

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'www/public')));
