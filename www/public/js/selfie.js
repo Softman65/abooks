35b691848fbdaa43af8cae4858791c5d8b80cd95
Softman65 <rbenitez2012@gmail.com>
2018-05-08 23:27:34 +0200

    push

--- www/public/js/selfie.js
+++ www/public/js/selfie.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     function imageForm(objdestino){
         $.fn.webCamera('photo',function(imgData){
             $('#images').modal('hide')
-            $(objdestino).attr("src",imgData).removeClass('hidden')
+            $(objdestino).attr("src",imgData).removeClass('hidden').show()
 
            // $.ajax({
            //     type: "POST",
