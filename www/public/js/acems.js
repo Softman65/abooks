f7fd5b01e580d4397b103dca5c9921a78c4866b1
Softman65 <rbenitez2012@gmail.com>
2018-05-10 12:36:24 +0200

    push

--- www/public/js/acems.js
+++ www/public/js/acems.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
     $('.ui.pointing.menu a.item').click(function(){
+      
         $('.ui.pointing.menu .item').removeClass('active')
         $(this).addClass('active')
         
-        $('.context.acems .ui.sidebar')
-        .sidebar({
-          context: $('.context.acems .bottom.segment')
-        })
-        .sidebar('attach events', '.context.acems .menu .item')
+        if($(this).attr("data")=="inicio"){
+
+        } 
+
+        if($(this).attr("data")=="directorio"){
+          $('.context.acems .ui.sidebar')
+          .sidebar({
+            context: $('.context.acems .bottom.segment')
+          })
+          .sidebar('attach events', '.context.acems .menu .item')          
+        } 
+
+        if($(this).attr("data")=="buscar"){
+          
+        } 
+       
     })
   
 })
