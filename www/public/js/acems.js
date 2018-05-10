a9531d3b7e6558e20396d45b6d978529b4d17915
Softman65 <rbenitez2012@gmail.com>
2018-05-10 12:43:17 +0200

    push

--- www/public/js/acems.js
+++ www/public/js/acems.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
       
         $('.ui.pointing.menu .item').removeClass('active')
         $(this).addClass('active')
-        $('.ui.basic.segment').addClass('hidden')
+        $('.ui.basic.segment .ui.basic.segment').addClass('hidden')
 
         if($(this).attr("data")=="inicio"){
           
-          $('.ui.basic.segment.inicio').removeClass('hidden')
+          $('.ui.basic.segment .ui.basic.segment.inicio').removeClass('hidden')
         } 
 
         if($(this).attr("data")=="directorio"){
-          $('.ui.basic.segment.directorio').removeClass('hidden')
+          $('.ui.basic.segment .ui.basic.segment.directorio').removeClass('hidden')
           $('.context.acems .ui.sidebar')
           .sidebar({
             context: $('.context.acems .bottom.segment')
