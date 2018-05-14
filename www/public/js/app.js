b79cb1a271d9805f157cba4831e1eef913474996
Softman65 <rbenitez2012@gmail.com>
2018-05-15 00:02:06 +0200

    push

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -325,9 +325,9 @@ $(document).ready(function() {
                         return value==null?null:$('<i class="'+_t+' icon sale">');
                     }},
                     {  title: "IBER", name: "C_iberlibro", type: "text", width: 40,filtering: false,
-                    itemTemplate: function(value) {
+                    itemTemplate: function(value,record) {
                         var _t = value>0?'green':'red'       
-                        return value==null?null:$('<i class="leanpub '+_t+' icon">');
+                        return value==null?null:$('<i class="amazon '+_t+' icon '+(record._sale!=null?'hidden':'')+'">');
                     }},
                     {  title: "AMAZON", name: "C_amazon", type: "text", width: 40,filtering: false,
                     itemTemplate: function(value,record) {
