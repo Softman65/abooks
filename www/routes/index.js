421a7924a5c810bbefe3fec65de029338e1e3626
Softman65 <rbenitez2012@gmail.com>
2018-05-16 09:19:00 +0200

    push

--- www/routes/index.js
+++ www/routes/index.js
@@ -185,8 +185,8 @@ router.get('/api/books/page', function (req, res) {
     var cadsql = "SELECT count(*) as total FROM books " + filter + ";SELECT count(*) as total FROM iberlibro " + filter + ";SELECT count(*) as total FROM amazon " + filter + ";SELECT "+ (fields + join + filter + order) + (filter.length==0? " LIMIT "+( _pageSize *(_pageIndex-1))+","+_pageSize:'')
     console.log(req.query.type)
     console.log(cadsql)
-    _pageSize = req.query.pageSize!=null?req.query.pageSize:10
-    _pageIndex = req.query.pageIndex!=null?req.query.pageIndex:1
+    const _pageSize = req.query.pageSize!=null?req.query.pageSize:10
+    const _pageIndex = req.query.pageIndex!=null?req.query.pageIndex:1
     mysql.connection.query(cadsql, function(err,records) {
          res.json({err:err,cadsql : cadsql,data:records[3],itemsCount:records[0][0].total*1,iberlibro:records[1][0].total*1,amazon:records[2][0].total*1});
          //debugger
