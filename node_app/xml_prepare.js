f7d9e9d87f4fd9793cf06b0e1f3db7e82fd15542
Softman65 <rbenitez2012@gmail.com>
2018-07-06 20:36:09 +0200

    .

--- node_app/xml_prepare.js
+++ node_app/xml_prepare.js
@@ -16,35 +16,42 @@ module.exports = function () {
             iberRecord += '<password>'+this.apiKey+'</password>' + newLine
             iberRecord += '</action>' + newLine
             iberRecord += '<AbebookList>' + newLine
+
             iberRecord += '<Abebook>' + newLine
+            iberRecord += '<transactionType>delete</transactionType>' + newLine
+            iberRecord +=  (_v(libro.vendorListingid)?'<vendorBookID>'+libro.vendorListingid+'</vendorBookID>':'')+ newLine
+            iberRecord += '</Abebook>'+ newLine
+            iberRecord += '<Abebook>' + newLine
+
+
             if(libro.vendorListingid.length>0){                
-                iberRecord +=  '<transactionType>'+accion+'</transactionType>' + newLine
-                iberRecord = iberRecord + (_v(libro.vendorListingid)?'<vendorBookID>'+libro.vendorListingid+'</vendorBookID>':'')+ newLine
-                iberRecord = iberRecord + (_v(libro.title)?'<title>'+libro.title+'</title>':'')+ newLine
-                iberRecord = iberRecord + (_v(libro.author)?'<author>'+libro.author+'</author>':'')+ newLine
-                iberRecord = iberRecord + (_v(libro.publisherName)?'<publisher>'+libro.publisherName+'</publisher>':'')+ newLine
-                iberRecord = iberRecord + (_v(libro.subject)?'<subject>'+libro.subject+'</subject>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.price_currency)?'<price currency="'+libro.price_currency+'">'+libro.price_quantity+'</price>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.bindingText)?'<binding>'+libro.bindingText+'</binding>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.description)?'<description>'+libro.description+'</description>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.bookCondition)?'<bookCondition>'+libro.bookCondition+'</bookCondition>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.jacketCondition)?'<jacketCondition>'+libro.jacketCondition+'</jacketCondition>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.universalIdentifier_number)?'<isbn>'+libro.universalIdentifier_number+'</isbn>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.publishPlace)?'<publishPlace>'+libro.publishPlace+'</publishPlace>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.publishYear)?'<publishYear>'+libro.publishYear+'</publishYear>':'') + newLine
-                iberRecord = iberRecord + (_v(libro.edition)?'<edition>'+libro.publishYear+'</edition>':'') + newLine
-                iberRecord = iberRecord + '<quantity  amount="1"></quantity>' + newLine
+                iberRecord +=  '<transactionType>add</transactionType>' + newLine
+                iberRecord += (_v(libro.vendorListingid)?'<vendorBookID>'+libro.vendorListingid+'</vendorBookID>':'')+ newLine
+                iberRecord += (_v(libro.title)?'<title>'+libro.title+'</title>':'')+ newLine
+                iberRecord += (_v(libro.author)?'<author>'+libro.author+'</author>':'')+ newLine
+                iberRecord += (_v(libro.publisherName)?'<publisher>'+libro.publisherName+'</publisher>':'')+ newLine
+                iberRecord += (_v(libro.subject)?'<subject>'+libro.subject+'</subject>':'') + newLine
+                iberRecord += (_v(libro.price_currency)?'<price currency="'+libro.price_currency+'">'+libro.price_quantity+'</price>':'') + newLine
+                iberRecord += (_v(libro.bindingText)?'<binding>'+libro.bindingText+'</binding>':'') + newLine
+                iberRecord += (_v(libro.description)?'<description>'+libro.description+'</description>':'') + newLine
+                iberRecord += (_v(libro.bookCondition)?'<bookCondition>'+libro.bookCondition+'</bookCondition>':'') + newLine
+                iberRecord += (_v(libro.jacketCondition)?'<jacketCondition>'+libro.jacketCondition+'</jacketCondition>':'') + newLine
+                iberRecord += (_v(libro.universalIdentifier_number)?'<isbn>'+libro.universalIdentifier_number+'</isbn>':'') + newLine
+                iberRecord += (_v(libro.publishPlace)?'<publishPlace>'+libro.publishPlace+'</publishPlace>':'') + newLine
+                iberRecord += (_v(libro.publishYear)?'<publishYear>'+libro.publishYear+'</publishYear>':'') + newLine
+                iberRecord += (_v(libro.edition)?'<edition>'+libro.publishYear+'</edition>':'') + newLine
+                iberRecord += '<quantity  amount="1"></quantity>' + newLine
                 debugger
                 if(imagenes.length>0){
-                    iberRecord = iberRecord + '<pictureList>' + newLine
+                    iberRecord =  '<pictureList>' + newLine
                     _.each(imagenes, function(_rPictures){
-                        iberRecord = iberRecord + '<pictureURL>'+_rPictures.idpictures +'.jpg<pictureURL>' + newLine
+                        iberRecord =  '<pictureURL>'+_rPictures.idpictures +'.jpg<pictureURL>' + newLine
                     })
-                    iberRecord = iberRecord + '</pictureList>' + newLine
+                    iberRecord =  '</pictureList>' + newLine
                 }
-                iberRecord = iberRecord + '</Abebook>'+ newLine
-                iberRecord = iberRecord + '</AbebookList>'+ newLine
-                iberRecord = iberRecord + '</inventoryUpdateRequest>'+ newLine
+                iberRecord +=  '</Abebook>'+ newLine
+                iberRecord +=  '</AbebookList>'+ newLine
+                iberRecord +=  '</inventoryUpdateRequest>'+ newLine
             }else{
                 iberRecord = null
             }
