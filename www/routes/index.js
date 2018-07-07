3e650d95e38a78adf12fc834326260207439d646
Softman65 <rbenitez2012@gmail.com>
2018-07-07 10:56:04 +0200

    .

--- www/routes/index.js
+++ www/routes/index.js
@@ -106,7 +106,7 @@ mysql.connection.connect(function(err) {
         router.post('/api/books/edit', function (req, res) {
             if(req.query.form=='formIberlibro'){
                 debugger
-                iberlibro.askToDb(req.query.vendorListingid,req.body.price_quantity_Iberlibro,function(_IberRecord,response){
+                iberlibro.askToDb(req.query.vendorListingid,req.body.price_quantity_Iberlibro*1,function(_IberRecord,response){
                     if(_IberRecord[0][0].price_quantity>0){
                         cadsql = "INSERT INTO iberlibro (vendorListingid,price_quantity,fecha_add) VALUES (?,?,NOW())  ON DUPLICATE KEY UPDATE price_quantity=?"
                     }else{
