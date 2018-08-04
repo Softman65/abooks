71eee3fa670535c0c2775fb77ed95afe73a9c8ba
Softman65 <rbenitez2012@gmail.com>
2018-08-04 13:34:36 +0200

    .

--- www/routes/index.js
+++ www/routes/index.js
@@ -251,8 +251,8 @@ mysql.connection.connect(function(err) {
             router.get('/api/iberlibro/delete', function (req, res) {
                 var _e = req.query.e*1
                 var _t = req.query.t*1
-                var cadsql = "SELECT (@cnt := @cnt + 1) AS rowNumber, t.* FROM abooks.iberlibro as t  CROSS JOIN (SELECT @cnt := 0) AS dummy ORDER by @cnt desc LIMIT ?,1"
-                mysql.connection.query(cadsql,[_e], function(err,records) {
+                var cadsql = "SELECT (@cnt := @cnt + 1) AS rowNumber, t.* FROM abooks.iberlibro as t  CROSS JOIN (SELECT @cnt := 0) AS dummy ORDER by @cnt desc LIMIT "+_e+",1"
+                mysql.connection.query(cadsql, function(err,records) {
                     if(err)
                         console.log(err)
                     console.log(records)
