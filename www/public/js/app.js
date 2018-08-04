88310a95ebaae472d68e4c5fb218f0dd1828fb74
Softman65 <rbenitez2012@gmail.com>
2018-08-05 00:38:21 +0200

    .

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -79,19 +79,6 @@ $(document).ready(function() {
             $('.bookfinder.'+form).removeClass('loading').html($data)
         })
     }
-    function forceKeyPressUppercase(e){
-        var charInput = e.keyCode;
-        if((charInput >= 97) && (charInput <= 122)) { // lowercase
-        if(!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKEY) { // no modifier key
-            var newChar = charInput - 32;
-            var start = e.target.selectionStart;
-            var end = e.target.selectionEnd;
-            e.target.value = e.target.value.substring(0, start) + String.fromCharCode(newChar) + e.target.value.substring(end);
-            e.target.setSelectionRange(start+1, start+1);
-            e.preventDefault();
-        }
-        }
-    }
     function validateform(){
         var s = [ 
             $('#edit input[name="price_quantity"]').val().length>0,
