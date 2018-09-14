5401d25fca1699f1e52cc64503963be69441b8c1
Ricardo Benitez <rbenitez2012@gmail.com>
2018-09-14 19:44:03 +0200

    .

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -449,7 +449,8 @@ $(document).ready(function() {
                     }
                 },
                 rowRenderer: function(item) {
-                    return $("<tr>").addClass(item._sale!=null?"custom-row":'')//.append();
+                    
+                    return $("<tr>").addClass(item._sale!=null?"custom-row":'').append($("<td>").append(item.Name));
                 },
                 fields: [
                     { title: "id", name: "idbooks", type: "number", width: 25, visible:false },
